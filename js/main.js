41ced74a9089199030e985a5f0d2dbb89ac83227
Deepkumar Patel <dspguru97@gmail.com>
2020-04-05 14:15:31 -0400

    textScrollAppearDone

--- js/main.js
+++ js/main.js
@@ -12,7 +12,48 @@ function openMenu()
     barImage.classList.toggle('rotateBurger');
 }
 
+//  PRODUCT PAGE - Scroll Text Appear effect function
+
+function  scrollAppear()
+{
+  var appearCon = document.querySelector(".appearCon");
+  var contentPosition = appearCon.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 1.5;
+
+  if(contentPosition < screenPosition)
+  {
+    appearCon.classList.add("appearText");
+  }
+}
+
+function  scrollAppear()
+{
+  var appearCon = document.querySelector(".appearCon");
+  var contentPosition = appearCon.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 1.5;
+
+  if(contentPosition < screenPosition)
+  {
+    appearCon.classList.add("appearText");
+  }
+}
+
+function  scrollAppearA()
+{
+  var appearCon = document.querySelector(".appearConA");
+  var contentPosition = appearCon.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 1.5;
+
+  if(contentPosition < screenPosition)
+  {
+    appearCon.classList.add("appearText");
+  }
+}
+//  PRODUCT PAGE - Scroll Text Appear effect function
+
 
 burger.addEventListener('click',openMenu);
+window.addEventListener('scroll', scrollAppear);
+window.addEventListener('scroll', scrollAppearA);
 
 })();
